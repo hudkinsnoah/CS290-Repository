ca41b4331f58c1b7d9975c6adbc889a3441c0431
hudkinsnoah <hudkinsn@oregonstate.edu>
2021-06-02 23:15:51 -0700

    minor changes

--- HW6/main.js
+++ HW6/main.js
@@ -11,7 +11,7 @@ app.set('port', 7957);
 
 app.get('/',function(req,res,next){
   var context = {};
-  mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+  mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
     if(err){
       next(err);
       return;
