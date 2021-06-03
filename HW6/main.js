5d47b51e20435c9b62fc24c4bd86b8e9984352a6
hudkinsnoah <hudkinsn@oregonstate.edu>
2021-06-02 23:13:28 -0700

    minor changes

--- HW6/main.js
+++ HW6/main.js
@@ -23,7 +23,7 @@ app.get('/',function(req,res,next){
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  [your connection pool].query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
     "id INT PRIMARY KEY AUTO_INCREMENT,"+
     "name VARCHAR(255) NOT NULL,"+
