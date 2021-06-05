9cf608e9dba2cd64df3e9048f3a5524323f0217d
hudkinsnoah <hudkinsn@oregonstate.edu>
2021-06-04 19:05:10 -0700

    Update dbcon.js

--- HW6/dbcon.js
+++ HW6/dbcon.js
@@ -9,4 +9,4 @@ var pool = mysql.createPool({
 
 module.exports.pool = pool;
 
-  mysql("INSERT INTO workouts (name, reps, weight, date, pounds) VALUES (row, 1, 40, 03-05-2018)")
+"INSERT INTO workouts (name, reps, weight, date, pounds) VALUES (row, 1, 40, 03-05-2018)")
